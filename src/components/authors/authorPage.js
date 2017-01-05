e08bdbbd9693cb6031229a0c9df31c6d76bd43df
wayofthepie <wayofthepie@gmail.com>
2017-01-05 19:39:25 +0000

    Add mock api calls and display

--- src/components/authors/authorPage.js
+++ src/components/authors/authorPage.js
@@ -7,10 +7,38 @@ let React = require('react');
 let AuthorApi = require('../../api/authorApi');
 
 class Authors extends React.Component {
+  getInitialState() {
+    return {
+      authors: []
+    };
+  }
+
+  componentWillMount() {
+    this.setState({authors: AuthorApi.getAllAuthors()});
+  }
+
   render() {
+    let createAuthorRow = function (author) {
+      return (
+        <tr key={author.id}>
+          <td><a href={'/#authors/' + author.id}>{author.id}</a></td>
+          <td>{author.firstName} {author.lastName}</td>
+        </tr>
+      )
+    };
+
     return (
       <div>
         <h1>Authors</h1>
+        <table className='table'>
+          <thead>
+          <th>ID</th>
+          <th>Name</th>
+          </thead>
+          <tbody>
+          {this.state.authors.map(createAuthorRow, this)}
+          </tbody>
+        </table>
       </div>
     );
   }
