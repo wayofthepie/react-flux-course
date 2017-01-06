49a41f8d18d1bcf4a18efe26fce6fa87e8750c6a
wayofthepie <wayofthepie@gmail.com>
2017-01-06 11:37:17 +0000

    Add an onLeave example

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import React from 'react';
 import { Redirect, Router, Route, Link, IndexRoute } from 'react-router';
 let About = require('./components/about/aboutPage');
 
+// A sample transition to.
 let onEnter = (nextState, transition, callback) => {
   if(!confirm('Are you sure?')) {
     transition.abort();
@@ -12,11 +13,18 @@ let onEnter = (nextState, transition, callback) => {
   }
 };
 
+// A sample transition from.
+let onLeave = (prevState) => {
+  if(!confirm('Are you sure?')) {
+    console.log(prevState);
+  }
+};
+
 let routes = (
   <Route path='/' component={require('./components/app')} >
     <IndexRoute component={require('./components/homePage')} />
     <Route path='authors' component={require('./components/authors/authorPage')} />
-    <Route path='about' component={About} onEnter={onEnter} />
+    <Route path='about' component={About} onEnter={onEnter} onLeave={onLeave} />
     <Redirect from='about-us' to='about' />
     <Route path='*' component={require('./components/notFound')} />
   </Route>
